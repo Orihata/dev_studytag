fc7d76e6dfea7a15de4d0740429708f0c8d1cf21
[Orihata] <[overture76@gmail.com]>
2019-10-01 17:54:28 +0900

    devise it works!

--- db/migrate/20191001083312_add_nickname_to_user.rb
+++ db/migrate/20191001083312_add_nickname_to_user.rb
@@ -1,5 +1,5 @@
 class AddNicknameToUser < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :user, :string
+    add_column :users, :nickname, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_01_083151) do
+ActiveRecord::Schema.define(version: 2019_10_01_083312) do
 
   create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 2019_10_01_083151) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
