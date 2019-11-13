65e887b57ed67ca97a440ee405e38f08cd83e212
[Orihata] <[overture76@gmail.com]>
2019-11-13 19:23:20 +0900

    test

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -2,7 +2,7 @@ class GenresController < ApplicationController
 
   def show
     @genre = Genre.find(params[:id])
-    @tweets = Tweet.where(genre_id: params[:id]).order("id DESC").page(params[:page]).per(6)
+    @tweets = Tweet.includes(:user,:genre).where(genre_id: params[:id]).order("id DESC").page(params[:page]).per(6)
   end
 
 end
